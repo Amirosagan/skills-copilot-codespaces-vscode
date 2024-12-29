3eb2654a952ee40791979d8dcb79fedd4c2ac8f0
Amir Elsagan <53624184+Amirosagan@users.noreply.github.com>
2024-12-29 18:08:10 +0000

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,3 +1,4 @@
+// Create web server
 const http = require('http');
 
 const hostname = '127.0.0.1';
@@ -12,5 +13,3 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
-
-// create
